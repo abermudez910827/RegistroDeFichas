6afe6c5163015ab73d2eeae866e67eb6ec0afc02
abermudez910827 <abermudez910827@gmail.com>
2020-08-03 23:42:38 -0400

    Añadida la confirmación de eliminación

--- registro_app/filters.py
+++ registro_app/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import QSE,Convenio
+from .models import QSE,Convenio,Obra
 
 
 class QSEFilter(django_filters.FilterSet):
@@ -10,9 +10,10 @@ class QSEFilter(django_filters.FilterSet):
 
 
 class ConvenioFilter(django_filters.FilterSet):
+    obra=django_filters.ModelChoiceFilter(queryset=Obra.objects.all())
     codigo = django_filters.CharFilter(lookup_expr='icontains')
     descripcion = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Convenio
-        fields = ['codigo','descripcion']
+        fields = ['obra','codigo','descripcion']
