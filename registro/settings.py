c2dfd122ff8013b8ca59af3096957b8e284d9974
abermudez910827 <abermudez910827@gmail.com>
2020-08-17 11:54:35 -0400

    Update settings.py

--- registro/settings.py
+++ registro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
